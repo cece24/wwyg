2e7d8747fd39b3ba4ccf20d2a0e17130c8bc2970
Cecilia Wong <ceciliakhwong@gmail.com>
2019-02-27 01:22:38 -0500

    Changes header logo styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,40 +7,34 @@ const HeaderContainer = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  z-index: 100;
-  background: #fff;
-  height: 5rem;
+  background: transparent;
   padding: 0.5rem 1rem;
+  z-index: 103;
 `
 
 const TitleContainer = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
+  display: flex;
+  justify-content: center;
 `
 
 const Title = styled.h1`
   font-family: 'Amatic SC', sans-serif;
-  background: linear-gradient(to right, #43e8d8 5%, #f54966 40%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+  font-size: 3rem;
+  color: #fff;
   margin: 0;
   margin-left: 2rem;
-  font-size: 3rem;
   width: 20rem;
 `
 
-const TitleOverlay = styled.h1`
+const SecondaryTitle = styled.h1`
   font-family: 'La Belle Aurore', sans-serif;
-  // background: linear-gradient(to right, #43e8d8 5%, #7f7f7f 40%);
-  background-color: rgba(137, 236, 218, 0.8);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+  font-size: 2.5rem;
+  line-height: 5rem;
+  color: rgba(137, 236, 218, 0.8);
   position: relative;
+  width: 100%;
   top: -2.1rem;
   left: 5rem;
-  font-size: 2.5rem;
-  width: 100%;
-  line-height: 5rem;
 `
 
 const Header = ({ siteTitle }) => (
@@ -52,7 +46,7 @@ const Header = ({ siteTitle }) => (
         }}
         to="/">
         <Title>{siteTitle}</Title>
-        <TitleOverlay>By Cece</TitleOverlay>
+        <SecondaryTitle>By Cece</SecondaryTitle>
       </Link>
     </TitleContainer>
   </HeaderContainer>
